9cb98e0a23d5f7a1be5cb6eb60d1536098c7f44b
megahomyak <g.megahomyak@gmail.com>
2023-11-29 11:39:07 +0500

    Updated the thing. I should really stick to simple scripts

--- cheech/__init__.py
+++ cheech/__init__.py
@@ -1,6 +1,24 @@
-class _Action:
-    def __init__(self, function):
-        self.function = function
+from argparse import ArgumentParser
 
-def action(function):
-    return _Action(function)
+class WithDefault:
+    def __init__(self, argname, default_value):
+        self.argname = argname
+        self.default_value = default_value
+
+def require(*argnames):
+    argparser = ArgumentParser()
+    plain_argnames = []
+    for argname in argnames:
+        if isinstance(argname, WithDefault):
+            plain_argnames.append(argname.argname)
+            argparser.add_argument(argname.argname, default=argname.default_value)
+        elif isinstance(argname, str):
+            plain_argnames.append(argname)
+            argparser.add_argument(argname)
+        else:
+            raise TypeError(f"encountered an argument name of an incompatible type {type(argname)}")
+    args = argparser.parse_args()
+    return [
+        getattr(args, plain_argname)
+        for plain_argname in plain_argnames
+    ]
